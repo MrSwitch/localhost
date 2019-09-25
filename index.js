29420c418b7dea9ff22405247cd8597d321bc6d0
Andrew Dodson <andrew@5app.com>
2019-09-25 13:57:57 +0100

    feat(mappath): Add mapping so ./:/urlpath/

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ var serveStatic = require('serve-static');
 var finalhandler = require('finalhandler');
 
 
-module.exports = function(directory) {
+module.exports = function(mapping) {
+
+	const [directory, basePath] = mapping.split(':');
 
 	// Serve up the file
 	var serve = serveStatic(directory);
 
 	// Create server
-	return http.createServer(function(req, res) {
+	return http.createServer((req, res) => {
 		// This is what its all about getting static files regardless of the HTTP method.
 		req.method = 'GET';
-		var done = finalhandler(req, res);
+
+		// Strip the basePath
+		if (basePath && req.url.startsWith(basePath)) {
+			req.url = req.url.slice(basePath.length);
+		}
+		const done = finalhandler(req, res);
 		serve(req, res, done);
 	});
 }
