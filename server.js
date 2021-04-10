2f7f054d3546784ddde5709c637e97f57bbb7e69
Andrew Dodson <947163+MrSwitch@users.noreply.github.com>
2021-04-10 23:12:08 +0100

    fix(ci): init lint and sem-rel (#1)
    * chore(lint): init lint
    
    * fix(ci): init lint and  sem-rel

---
+++ server.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+const localhost = require('./index.js');
+const portfinder = require('portfinder');
+const argv = require('optimist')
+	.boolean('cors')
+	.argv;
+
+const port = argv.p || parseInt(process.env.PORT);
+
+if (!port) {
+	portfinder.basePort = 8080;
+	portfinder.getPort((err, port) => {
+		if (err) {
+			throw err;
+		}
+		listen(port);
+	});
+}
+else {
+	listen(port);
+}
+
+function listen(port) {
+	localhost(argv._[0] || './').listen(port);
+	console.log(`Starting //localhost${port !== 80 ? `:${port}` : ''}`);
+}
+
+if (process.platform === 'win32') {
+	require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout
+	}).on('SIGINT', () => {
+		process.emit('SIGINT');
+	});
+}
+
+process.on('SIGINT', () => {
+	console.info('localhost stopped.'.red);
+	process.exit();
+});
+
+process.on('SIGTERM', () => {
+	console.info('localhost stopped.'.red);
+	process.exit();
+});
